9a8ccfb97d6a53f1ea33b42d1bc731ea58c0583e
Javierdz <javierdz@javierdz.xyz>
2024-04-09 09:17:34 +0200

    lentejas

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 const titulo = document.getElementsByClassName("titulo");
 
 function onClick(){
-    window.location.href = "https://"
+    window.location.href = "https://www.youtube.com/watch?v=5NIA8Ef5C-Q"
 }
